251d642a85f9a361c8864590c62859fc4114ac75
Andy Li <andy@onthewings.net>
2015-12-31 17:58:26 +0800

    Fixed Java Process argument escaping.

--- std/java/_std/sys/io/Process.hx
+++ std/java/_std/sys/io/Process.hx
@@ -41,10 +41,10 @@ class Process {
 		var pargs = new NativeArray(args.length + 1);
 		switch (Sys.systemName()) {
 			case "Windows":
-				pargs[0] = StringTools.quoteWinArg(cmd);
+				pargs[0] = StringTools.quoteWinArg(cmd, false);
 				for (i in 0...args.length)
 				{
-					pargs[i + 1] = StringTools.quoteWinArg(args[i]);
+					pargs[i + 1] = StringTools.quoteWinArg(args[i], false);
 				}
 			case _:
 				pargs[0] = cmd;
